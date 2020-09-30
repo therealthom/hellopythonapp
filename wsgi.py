f1f8498c2eb8d25e321b0373570e96a348041bfb
Tomas Moreno Plaza <tmorenopl@gmail.com>
2020-09-30 11:41:55 -0500

    fix

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return "Hola, Tepic!\r\n", 200, { 'Content-Type': 'text/plain' }
+    return "Hola, Toluca!\r\n", 200, { 'Content-Type': 'text/plain' }
 
 if __name__ == '__main__':
     application.run(debug = True)
